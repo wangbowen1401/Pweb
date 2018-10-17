137c44b61b51058c1e687311c303a08cf29bded8
wangbowen1401 <wangbowen0204@gmail.com>
2018-10-16 22:09:33 -0400

    Update app.js

---
+++ app.js
@@ -0,0 +1,27 @@
+var text = ["Chemical Engineer", "Programmer"];
+var counter = 0;
+var elem = document.getElementById("changeText");
+
+function change() {
+  $(elem).fadeTo(1200,0,function(){
+  	this.innerHTML=text[counter];
+  	counter = ++counter % text.length;
+  	$(this).fadeTo(1200,1,change);
+  })
+}
+
+change()
+
+///////////////////////// Tab Portions//////////////////////
+
+var tabs = document.querySelectorAll(".tabContainer.HoverButton a");
+var tabPanels=document.querySelectorAll(".tabContainer  .tabPanel");
+
+function showPanel(panelIndex,colorCode) {
+    tabPanels.forEach(function(node){
+        node.style.display="none";
+    });
+    tabPanels[panelIndex].style.display="block";
+}
+
+showPanel(0);
