d5efc37f368aecbecf27f4dbcdfd6ecaa633885f
wangbowen1401 <wangbowen0204@gmail.com>
2018-10-26 15:40:05 -0400

    fadeIn fadeOut for panels

--- app.js
+++ app.js
@@ -16,10 +16,25 @@ change()
 ///////////////////////// Tab Portions//////////////////////
 
 $(document).ready(function(){
-  console.log("ready!");
-  $("#HomePanel").show();
-
-  $(".HoverButton").click(function(){
-    $(this);
-  });
+	var panels=["#HomePanel","#AboutMePanel"];
+	console.log("ready!");
+	$("#HomePanel").show();
+	$(".HoverButton").click(function(){
+		panels.forEach(function(panel){
+			$(panel).fadeOut(300).promise().done();
+		});
+		$(this.getAttribute('tab')).delay(100);
+		$(this.getAttribute('tab')).fadeIn(600);
+	});
 });
+
+/*
+if($(this).attr("tab")=="Home"){
+			if($("#AboutMePanel").is(":visible"))
+				$("#AboutMePanel").fadeOut(300,function(){$("#HomePanel").fadeIn(300)});
+		}
+if($(this).attr("tab")=="AboutMe"){
+	if($("#HomePanel").is(":visible"))
+		$("#HomePanel").fadeOut(300,function(){$("#AboutMePanel").fadeIn(300)});
+}
+*/
